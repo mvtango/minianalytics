13ed1d9741efa0b3d877ad18e4558d1d64466f7b
Martin Virtel <martin.virtel@gmail.com>
2021-03-05 17:52:30 +0100

    ua category

--- reports/ua.sh
+++ reports/ua.sh
@@ -7,7 +7,22 @@ if [ "${DEBUG:-}" != "" ] ; then
 fi
 
 
-q -H --output-header --tab-delimited '
-   select count(*), ua from - 
-    group by 
-     ua '
+q -H --output-header --tab-delimited ' select count(*) as c, ua from - group by ua ' \
+| q -H --output-header --tab-delimited '
+   select 
+       ua,
+       c,
+       case 
+             when ua like "%Android%" then "mobile.android"
+             when ua like "%iPhone%" or ua like "%iPad%" then "mobile.ios"
+             when ua like "%OS X%" then "desktop.mac"
+             when ua like "%Linux%" then "desktop.linux"
+             when ua like "%Windows %" then "desktop.windows"
+             else "unknown"
+       end as system,
+       case 
+             when ua like "%bot%" or ua like "%crawl%" then "bot"
+             else "user"
+       end as category
+       from - 
+    '
