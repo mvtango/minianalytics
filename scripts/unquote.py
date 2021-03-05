cdb2690723d574bf4bf58a3ded4a8a5452ca2aad
Martin Virtel <martin.virtel@gmail.com>
2021-03-05 09:12:29 +0100

    better unqote

--- scripts/unquote.py
+++ scripts/unquote.py
@@ -1,19 +1,42 @@
+#! /usr/bin/env python3
+"""
+%(prog)s -- tsvfile processor
 
 
+USAGE: %(prog)s col1 [col2 ...]
+
+url_unquote columns
+
+"""
 import sys
+import os
 import re
+import csv
+import itertools
+from collections import OrderedDict
 import urllib.parse
 
-def process() :
-    for line in sys.stdin.readlines():
-        if "%0" in line or "%1" in line:
-            sys.stdout.write(line)
-        else:
-            unquoted = urllib.parse.unquote_plus(line)
-            sys.stdout.write(unquoted)
+
+def process(argl) :
+    sys.stderr.write(f"Unqoting columns: {argl}\n")
+    in_file=csv.DictReader(sys.stdin, delimiter="\t")
+    out_file=None
+    safestr = ''.join((chr(a) for a in range(ord(' '),255)))
+    for row in in_file:
+        for column in argl:
+            unquoted = urllib.parse.unquote_plus(row[column])
+            row[column] = re.sub("[\x00-\x1f]", lambda a: "%%%02x" % ord(a.group(0)), unquoted)
+        if out_file is None:
+            keys = list(row.keys())
+            out_file=csv.DictWriter(sys.stdout, keys, delimiter="\t")
+            out_file.writeheader()
+        out_file.writerow(row)
 
 def main():
-    process()
+    if len(sys.argv)<2 :
+        print(__doc__ % dict(prog=sys.argv[0]))
+    else :
+        process(sys.argv[1:])
 
 if __name__=='__main__' :
     main()
